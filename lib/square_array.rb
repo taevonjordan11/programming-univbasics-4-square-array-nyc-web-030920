29826e552491dcb97601a0601e1cfa4dd708cc97
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-25 19:08:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,12 +8,3 @@ def square_array(array)
 squared_array
 end
 
-def square_array(array)
-  counter = 0
-  squared_array = []
-  while counter < array.length do
-  squared_array << (array[counter] ** 2)
-  counter = counter + 1
- end
- squared_array
-end
